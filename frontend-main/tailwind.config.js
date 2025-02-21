37a81e7b69ffa2ce751fc8efc24b4c8e403ee838
Fabio83500 <fabio83500@live.fr>
2025-02-21 14:44:30 +0000

    first push

---
+++ frontend-main/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        marquee: 'marquee 40s linear infinite', // Durée de l'animation
+      },
+      keyframes: {
+        marquee: {
+          '0%': {
+            transform: 'translateX(0)',
+          },
+          '100%': {
+            transform: 'translateX(-100%)',
+          },
+        },
+      },
+      
+      boxShadow: {
+        "custom-lg": "0px 0px 15px 3px rgba(23,23,23, 0.20)",
+        "custom-md": "0px 0px 10px 2px rgba(23,23,23, 0.16)",
+        "custom-sm": "0px 0px 6px 2px rgba(23,23,23, 0.16)",
+      },
+    },
+  },
+  plugins: [],
+}
