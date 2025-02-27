fa19056cf994efc4e60833bff3a9a372c1d22623
Fabio83500 <fabio83500@live.fr>
2025-02-27 17:40:45 +0000

    font

--- frontend-main/pages/_app.js
+++ frontend-main/pages/_app.js
@@ -1,18 +1,11 @@
 import "@/styles/globals.css";
 import Layout from "../components/Layout/layout";
-import { Poppins } from 'next/font/google'
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--poppins-font",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  style: ["normal"],
-});
+
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout className={poppins.className}>
+    <Layout >
       <Component {...pageProps} />
     </Layout>
   );
